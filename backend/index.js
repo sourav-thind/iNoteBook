cda4fe43bcc73d4ba05374b3a64840710e779d75
Souravpreet Singh <souravpreetsinghpb41@gmail.com>
2023-04-30 15:48:21 -0400

    vercel.json and env.local file

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const connectToMongo = require('./db');
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+require('dotenv').config();
+
 
 connectToMongo();
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
