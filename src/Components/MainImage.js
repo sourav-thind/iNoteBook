d87a330c43de8e695d230381d2d4ae0ad03a49c7
Souravpreet Singh <souravpreetsinghpb41@gmail.com>
2023-06-01 05:25:12 -0400

    Main image responsive fix

--- src/Components/MainImage.js
+++ src/Components/MainImage.js
@@ -6,57 +6,61 @@ const MainImage = () => {
   return (
     <> 
     
-    <style type='text/css'> 
-    {
-        `
-       #img {
-            height:500px;
-            width: 90%;
-        }
-        .both{
-          
-            height:70px;
-            width:160px;
-            top: 74%;
-        }
-        .img-btn1{
-            left:36%
-        }
-        .img-btn2{
-            left: 48%
-        }
-        .third{
-          
-            height:70px;
-            width:350px;
-            top: 74%;
-        }
-        .h5{
-            top: 35%;
-            color:black;
-            left: 9%;
-        }
-        .p{
-            top: 61%;
-            color:black;
-            left:9%
-        }
-
-        `
+    <style type='text/css'>{`
+    #img {
+        height: auto;
+        max-width: 100%;
     }
-    </style>
+
+    // .both {
+    //     height: 70px;
+    //     width: 100%;
+    //     display: flex;
+    //     justify-content: space-between;
+    //     margin-top: 2%;
+    // }
+
+    .img-btn1 {
+        width: 40%;
+    }
+
+    .img-btn2 {
+        width: 40%;
+    }
+
+    .third {
+        height: 70px;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        margin-top: 2%;
+    }
+
+    .h5 {
+        color: black;
+        text-align: center;
+        margin-top: 5%;
+    }
+
+    .p {
+        color: black;
+        text-align: center;
+        margin-top: 5%;
+    }
+`}
+</style>
+
     <div >
      <Image src={require('./Images/Main.jpg')} className="mt-2 mx-0" id="img"  alt="Responsive image"/>
-     <h5 className='carousel-caption h5'>Store your notes in cloud and <br/> access them from anywhere</h5>
-     
-     {!localStorage.getItem('token')?<div>
+    
+     {/* {!localStorage.getItem('token')?<div className='flex flex-col'>
      <p className='carousel-caption p ' > Join our community by Signing up <br/>or Login if you already have an account</p>
       <Link className="btn btn-primary carousel-caption both img-btn1  "  to='/login'><h4>Login</h4></Link> 
      <Link className="btn btn-primary carousel-caption both img-btn2 " to='/signup'><h4>Sign UP</h4></Link></div>:
      <div>
         <p className='carousel-caption p ' >You are Logged in. <br/> Here are Your Notes</p>
         <Link className="btn btn-primary carousel-caption third img-btn1 " to='/Notes'><h4> Your Notes</h4></Link></div>
-     }
+     } */}
 
     </div>
     </>
